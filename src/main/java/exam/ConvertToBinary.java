e706f25b1df366de8f370e653fc565d032cf5cc8
TanyaJoshi5 <54098287+TanyaJoshi5@users.noreply.github.com>
2021-06-22 12:45:10 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,19 @@
 package exam;
-
+import java.util.*;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    int bin[]= new int[50];
+    int ind=0;
+    while(n>0){
+      bin[ind++]=n%2;
+      n=n/2;
+    }
+    for(int i=ind-1;i>=0;i--){
+      System.out.println(bin[i]);
+    }
+    System.out.println();
     return "";
   }
 }
